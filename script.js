5dca4bb51de34516035498af654da6c3cc061066
ValentTheBandit <jaro.benjamin.2005@gmail.com>
2025-04-01 14:41:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('trapForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Bemeneti értékek, parseFloat használatával biztosítva, hogy a tizedesjegyeket is befogadja
+    const a = parseFloat(document.getElementById('a').value);
+    const b = parseFloat(document.getElementById('b').value);
+    const c = parseFloat(document.getElementById('c').value);
+    
+    // Számítások
+    const x = c - a;
+    const d = Math.sqrt(b * b + x * x);
+    const alphaRad = Math.atan(x / b);
+    const tompaszog = (Math.degrees(alphaRad) + 90).toFixed(2);
+    
+    // Eredmények megjelenítése
+    document.getElementById('angle').textContent = tompaszog;
+    document.getElementById('dSide').textContent = d.toFixed(2);
+    
+    // Eredmények div megjelenítése
+    document.getElementById('result').style.display = 'block';
+});
+
+// Kiegészítés a Math.degrees metódushoz
+Math.degrees = function(radians) {
+    return radians * (180 / Math.PI);
+};
